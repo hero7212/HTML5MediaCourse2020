735e6a1e1e92dbd1f24632aefb1a004d682773bc
frontendgod <3054750147@qq.com>
2020-06-02 21:07:13 +0800

    p06

---
+++ UsingCamera/app.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: "#vueapp",
+
+    mounted() { 
+        this._initVueApp()
+    },
+
+    methods: {
+
+        async _initVueApp() {
+            this.$refs.video.srcObject = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+
+            this._context2d = this.$refs.canvas.getContext('2d')
+        },
+
+        btnTakePhotoClicked() {
+            this._context2d.drawImage(this.$refs.video, 0, 0, 400, 300)
+        }
+    }
+})
+
+
+
+
+
+
